1f0854880afff7b3fb05b8e1f889eb8a7c3f700c
David Coppit <david@coppit.org>
2015-04-19 15:14:33 -0400

    Require File::Slurp instead of including it, to avoid potential problems

--- private-lib/Module/Install/PRIVATE/Add_Test_Target.pm
+++ private-lib/Module/Install/PRIVATE/Add_Test_Target.pm
@@ -14,9 +14,11 @@ $VERSION = sprintf "%d.%02d%02d", q/0.10.0/ =~ /(\d+)/g;
 
 sub Add_Test_Target
 {
-  my $self = shift;
-  my $target = shift;
-  my $test = shift;
+  my ($self, $target, $test) = @_;
+
+  # Tell Module::Install to include this, since we use it.
+  $self->perl_version('5.005');
+  $self->configure_requires('File::Slurp', 0);
 
   *main::MY::postamble = sub {
     return &Module::AutoInstall::postamble . <<EOF;
